c911be5094d423077ef035b115bc621d171be031
Stephanie Nemeth <traumverloren@gmail.com>
2017-11-24 18:35:13 +0100

    something kinda works

--- umbrella_rgbw_expt.ino
+++ umbrella_rgbw_expt.ino
@@ -2,28 +2,30 @@
 #include <Adafruit_NeoPixel.h> // Include the adafruit Neopixel Library
 #include <WebSocketsClient.h> // Include Socket.IO client library to communicate with Server!
 
-const char* ssid     = "";
-const char* password = "";
+const char* ssid     = "SSID";
+const char* password = "PASSWORD";
 
-const int length = 17;
+String currentProgram = "colorWipe";
 
-enum mode {modeColorWipe, modeRain, modeRainbow, modeSocketConnect};
-mode currentMode = modeSocketConnect;
+#define rowCount = rowCount;
+enum mode {colorWipe, modeSunrise, modeSunset, modeDay, modeNight, modeDayStorm, modeNightStorm, rainbow, rain};
+mode currentMode = colorWipe;
+
 // Parameter 1 = number of pixels in strip
-// Parameter 2 = pin number (most are valid)
+// Parameter 2 = pin number
 // Parameter 3 = pixel type flags, add together as needed:
 //   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip_1 = Adafruit_NeoPixel(length, 4, NEO_GRBW + NEO_KHZ800);
-Adafruit_NeoPixel strip_2 = Adafruit_NeoPixel(length, 5, NEO_GRBW + NEO_KHZ800);
-Adafruit_NeoPixel strip_3 = Adafruit_NeoPixel(length, 2, NEO_GRBW + NEO_KHZ800);  
-Adafruit_NeoPixel strip_4 = Adafruit_NeoPixel(length, 0, NEO_GRBW + NEO_KHZ800);  
-Adafruit_NeoPixel strip_5 = Adafruit_NeoPixel(length, 15, NEO_GRBW + NEO_KHZ800);
-Adafruit_NeoPixel strip_6 = Adafruit_NeoPixel(length, 12, NEO_GRBW + NEO_KHZ800);  
-Adafruit_NeoPixel strip_7 = Adafruit_NeoPixel(length, 13, NEO_GRBW + NEO_KHZ800);  
-Adafruit_NeoPixel strip_8 = Adafruit_NeoPixel(length, 14, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel strip_1 = Adafruit_NeoPixel(rowCount, 4, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel strip_2 = Adafruit_NeoPixel(rowCount, 5, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel strip_3 = Adafruit_NeoPixel(rowCount, 2, NEO_GRBW + NEO_KHZ800);  
+Adafruit_NeoPixel strip_4 = Adafruit_NeoPixel(rowCount, 0, NEO_GRBW + NEO_KHZ800);  
+Adafruit_NeoPixel strip_5 = Adafruit_NeoPixel(rowCount, 15, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel strip_6 = Adafruit_NeoPixel(rowCount, 12, NEO_GRBW + NEO_KHZ800);  
+Adafruit_NeoPixel strip_7 = Adafruit_NeoPixel(rowCount, 13, NEO_GRBW + NEO_KHZ800);  
+Adafruit_NeoPixel strip_8 = Adafruit_NeoPixel(rowCount, 14, NEO_GRBW + NEO_KHZ800);
 
 WiFiClient client;
 WebSocketsClient webSocket;
@@ -47,7 +49,7 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
             {
                 Serial.printf("[WSc] Connected to url: %s\n",  payload);
                 isConnected = true;
-                currentMode = modeColorWipe;
+
                 // send message to server when Connected
                 // socket.io upgrade confirmation message (required)
                 webSocket.sendTXT("5");
@@ -89,14 +91,14 @@ void trigger(const char* event, const char * payload, size_t length) {
   
   if(strcmp(event, "rainbow") == 0) {
     Serial.printf("[WSc] trigger event %s\n", event);
-    currentMode = modeRainbow;
+    currentProgram = "rainbow";
 //    shirtColors(payload, length);
   } else if (strcmp(event, "colorWipe") == 0){
      Serial.printf("[WSc] trigger event %s\n", event);
-     currentMode = modeColorWipe;
+     currentProgram = "colorWipe";
   } else if (strcmp(event, "rain") == 0){
      Serial.printf("[WSc] trigger event %s\n", event);
-     currentMode = modeRain;
+     currentProgram = "rain";
   }
 }
 
@@ -156,30 +158,28 @@ void setup() {
 
   delay(10);
   webSocket.beginSocketIO("led-umbrella.herokuapp.com", 80);
-  isConnected = true;   
   webSocket.onEvent(webSocketEvent);
+  delay(10);
 }
 
 void loop() {
   webSocket.loop();
-  
-  switch(currentMode)
-  {
-    case modeColorWipe:
-      Serial.print("colorWipe\n");
-      colorWipe(strip_1.Color(0, 0, 0, 255), 50); // White
-      break;
-    case modeRainbow:
-      Serial.print("rainbow\n");
-      colorWipe(strip_1.Color(255, 0, 0, 0), 50); // Red
-      break;
-    case modeRain:
-      Serial.print("rain\n");
-      rain();
-      break;
-    default:
-      break;
+
+  if (currentProgram == "colorWipe") {
+      
+//      colorWipe(strip_1.Color(0, 0, 0, 255), 50); // White
+//      colorWipe(strip_1.Color(255, 0, 0, 0), 50); // Red
+//      colorWipe(strip_1.Color(0, 255, 0, 0), 50); // Green
+//      colorWipe(strip_1.Color(0, 0, 255, 0), 50); // Blue
+  } else if (currentProgram == "rainbow") {
+      Serial.print("rainbow");
+  } else if (currentProgram == "rain" ) {
+      Serial.print("rain");
+  } else {
+    Serial.print("ELSE UH OH");
   }
+
+  delay(10);
 
   if(isConnected) {
 
@@ -202,8 +202,7 @@ void loop() {
       WiFi.begin(ssid, password);
       delay(10);
       webSocket.beginSocketIO("led-umbrella.herokuapp.com", 80);
-      isConnected = true;   
-      currentMode = modeSocketConnect;
+      isConnected = true;
     }    
 }
  
@@ -218,118 +217,96 @@ void colorWipe(uint32_t c, uint8_t wait) {
       strip_6.setPixelColor(i, c);
       strip_7.setPixelColor(i, c);
       strip_8.setPixelColor(i, c);
-      showStrips();
+      strip_1.show();
+      strip_2.show();
+      strip_3.show();
+      strip_4.show();
+      strip_5.show();
+      strip_6.show();
+      strip_7.show();
+      strip_8.show();
       delay(wait);
   }
-}
-
-void showStrips() {
-  strip_1.show();
-  strip_2.show();
-  strip_3.show();
-  strip_4.show();
-  strip_5.show();
-  strip_6.show();
-  strip_7.show();
-  strip_8.show();
 }
 // Rain Program
  
 void rain() {
-  int pos[17];
-    // Set each rain drop at the starting gate.
+  // Create an array of 20 raindrops
+  const int count = 20;
+  int pos[count];
+  // Set each rain drop at the starting gate.
   // Signify by a position of -1
-  for( int i=0; i < length; i++) {
+  for( int i=0; i < count; i++) {
     pos[i]=-1;
   }
-
+  // Main loop. Keep looping until we've done
+  // enough "frames."
   boolean done=false;
   int counter = 0;
   while(!done) {
-  // Start by turning all LEDs off:
-  for(int i=0; i<strip_1.numPixels(); i++) {
-    strip_1.setPixelColor(i, 0);
-    strip_2.setPixelColor(i, 0);
-    strip_3.setPixelColor(i, 0);
-    strip_4.setPixelColor(i, 0);
-    strip_5.setPixelColor(i, 0);
-    strip_6.setPixelColor(i, 0);
-    strip_7.setPixelColor(i, 0);
-    strip_8.setPixelColor(i, 0);  
-    showStrips();
-  }
-
-  // Loop for each rain drop
-  for( int i=0; i < length; i++) {
-  // If the drop is out of the starting gate,
-  // turn on the LED for it.
-    if( pos[i] >= 0 ) {
-      strip_1.setPixelColor(pos[i], strip_1.Color(0, 0, 127, 0));
-      strip_2.setPixelColor(pos[i], strip_1.Color(0, 0, 127, 0));
-      strip_3.setPixelColor(pos[i], strip_1.Color(0, 0, 127, 0));
-      strip_4.setPixelColor(pos[i], strip_1.Color(0, 0, 127, 0));
-      strip_5.setPixelColor(pos[i], strip_1.Color(0, 0, 127, 0));
-      strip_6.setPixelColor(pos[i], strip_1.Color(0, 0, 127, 0));
-      strip_7.setPixelColor(pos[i], strip_1.Color(0, 0, 127, 0));
-      strip_8.setPixelColor(pos[i], strip_1.Color(0, 0, 127, 0));
-      // Move the drop down one row
-      pos[i] -= 1;
+    // Start by turning all LEDs off:
+    for(int i=0; i<strip.numPixels(); i++)
+    strip.setPixelColor(i, 0);
+   
+    // Loop for each rain drop
+    for( int i=0; i < count; i++) {
+    // If the drop is out of the starting gate,
+    // turn on the LED for it.
+      if( pos[i] >= 0 ) {
+        strip.setPixelColor(pos[i], strip.Color(0,0,127));
+        // Move the drop down one row
+        pos[i] -= 7;
+      }
+      // If we've fallen off the strip, but us back at the starting gate.
+      if( pos[i] < 0 )
+        pos[i]=-1;
+      }
+      // If this drop is at the starting gate, randomly
+      // see if we should start it falling.
+      if ( pos[i] == -1 && random(40) == 0 && counter < 380 ) {
+        // Pick one of the 6 starting spots to begin falling
+        pos[i] = 143-random(6);
+      }
+      strip.show();
+      delay(2);
     }
-
-    // If we've fallen off the strip, but us back at the starting gate.
-    if( pos[i] < 0 ) {
-      pos[i]=-1;
-    }
-    // If this drop is at the starting gate, randomly
-    // see if we should start it falling.
-    if ( pos[i] == -1 && random(40) == 0 && counter < 380) {
-      // Pick one of the 6 starting spots to begin falling
-      pos[i] = 17-random(16);
-    } else if (counter >= 380) {
-      done = true;
-    }
-    counter += 1;
-    showStrips();
-    delay(2);
   }
-  }
-
 }
 //Rainbow Program
-//void rainbow(uint8_t wait) {
-//  uint16_t i, j;
-//  
-//  for(j=0; j<256; j++) {
-//    for(i=0; i<strip.numPixels(); i++) {
-//strip.setPixelColor(i, Wheel((i+j) & 255));
-//}
-//strip.show();
-//    delay(wait);
-//  }
-//}
-//  
-//// Rainbow Cycle Program - Equally distributed
-//void rainbowCycle(uint8_t wait) {
-//uint16_t i, j;
-//  
-//for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-//for(i=0; i< strip.numPixels(); i++) {
-//strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-//}
-//strip.show();
-//delay(wait);
-//}
-//}
-//// Input a value 0 to 255 to get a color value.
-//// The colours are a transition r - g - b - back to r.
-//uint32_t Wheel(byte WheelPos) {
-//if(WheelPos < 85) {
-//return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-//} else if(WheelPos < 170) {
-//WheelPos -= 85;
-//return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-//} else {
-//WheelPos -= 170;
-//return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-//}
-//}
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+  
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+strip.setPixelColor(i, Wheel((i+j) & 255));
+}
+strip.show();
+    delay(wait);
+  }
+}
+  
+// Rainbow Cycle Program - Equally distributed
+void rainbowCycle(uint8_t wait) {
+uint16_t i, j;
+  
+for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+for(i=0; i< strip.numPixels(); i++) {
+strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+}
+strip.show();
+delay(wait);
+}
+}
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+if(WheelPos < 85) {
+return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+} else if(WheelPos < 170) {
+WheelPos -= 85;
+return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+} else {
+WheelPos -= 170;
+return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+}
+}
