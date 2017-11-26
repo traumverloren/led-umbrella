51efa8aace29669d6c886f135af1e6a0e02c307f
Stephanie Nemeth <traumverloren@gmail.com>
2017-11-26 20:20:38 +0100

    code optimizations and added rainbow method

--- umbrella_rgbw.ino
+++ umbrella_rgbw.ino
@@ -5,10 +5,13 @@
 const char* ssid     = "ssid";
 const char* password = "pw";
 
-const int length = 17;
+const int stripCount = 8;
+const int ledCount = 17;
+const int brightness = 100;
 
-enum mode {modeColorWipe, modeRainbowRain, modeRain, modeRainbow, modeSocketConnect};
+enum mode {modeColorWipe, modeRainbowRain, modeRain, modeRainbow, modeSocketConnect, modeSparkle};
 mode currentMode = modeSocketConnect;
+
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
 // Parameter 3 = pixel type flags, add together as needed:
@@ -16,32 +19,28 @@ mode currentMode = modeSocketConnect;
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip_1 = Adafruit_NeoPixel(length, 4, NEO_GRBW + NEO_KHZ800);
-Adafruit_NeoPixel strip_2 = Adafruit_NeoPixel(length, 5, NEO_GRBW + NEO_KHZ800);
-Adafruit_NeoPixel strip_3 = Adafruit_NeoPixel(length, 2, NEO_GRBW + NEO_KHZ800);  
-Adafruit_NeoPixel strip_4 = Adafruit_NeoPixel(length, 0, NEO_GRBW + NEO_KHZ800);  
-Adafruit_NeoPixel strip_5 = Adafruit_NeoPixel(length, 15, NEO_GRBW + NEO_KHZ800);
-Adafruit_NeoPixel strip_6 = Adafruit_NeoPixel(length, 12, NEO_GRBW + NEO_KHZ800);  
-Adafruit_NeoPixel strip_7 = Adafruit_NeoPixel(length, 13, NEO_GRBW + NEO_KHZ800);  
-Adafruit_NeoPixel strip_8 = Adafruit_NeoPixel(length, 14, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel strip_1 = Adafruit_NeoPixel(ledCount, 4, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel strip_2 = Adafruit_NeoPixel(ledCount, 5, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel strip_3 = Adafruit_NeoPixel(ledCount, 2, NEO_GRBW + NEO_KHZ800);  
+Adafruit_NeoPixel strip_4 = Adafruit_NeoPixel(ledCount, 0, NEO_GRBW + NEO_KHZ800);  
+Adafruit_NeoPixel strip_5 = Adafruit_NeoPixel(ledCount, 15, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel strip_6 = Adafruit_NeoPixel(ledCount, 12, NEO_GRBW + NEO_KHZ800);  
+Adafruit_NeoPixel strip_7 = Adafruit_NeoPixel(ledCount, 13, NEO_GRBW + NEO_KHZ800);  
+Adafruit_NeoPixel strip_8 = Adafruit_NeoPixel(ledCount, 14, NEO_GRBW + NEO_KHZ800);
 
 Adafruit_NeoPixel pixelStrips[8] = {strip_1, strip_2, strip_3, strip_4, strip_5, strip_6, strip_7, strip_8};
-uint32_t strips[8][17];
-int stripCount = 8;
-int ledCount = 17;
+uint32_t strips[stripCount][ledCount];
 
-WiFiClient client;
 WebSocketsClient webSocket;
 
 #define MESSAGE_INTERVAL 10000
 #define HEARTBEAT_INTERVAL 5000
 
-
 uint64_t messageTimestamp = 0;
 uint64_t heartbeatTimestamp = 0;
 bool isConnected = false;
 
-void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t eventLength) {
     String msg;
     switch(type) {
         case WStype_DISCONNECTED:
@@ -50,27 +49,25 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
             currentMode = modeSocketConnect;
             break;
         case WStype_CONNECTED:
-            {
-                Serial.printf("[WSc] Connected to url: %s\n",  payload);
-                isConnected = true;
-                currentMode = modeColorWipe;
-                // send message to server when Connected
-                // socket.io upgrade confirmation message (required)
-                webSocket.sendTXT("5");
-            }
+            Serial.printf("[WSc] Connected to url: %s\n",  payload);
+            // send message to server when Connected
+            // socket.io upgrade confirmation message (required)
+            webSocket.sendTXT("5");
+            isConnected = true;
+            currentMode = modeSparkle;
             break;
         case WStype_TEXT:
             msg = String((char*)payload);
             if(msg.startsWith("42")) {
-              trigger(getEventName(msg).c_str(), getEventPayload(msg).c_str(), length);
+              trigger(getEventName(msg).c_str(), getEventPayload(msg).c_str(), eventLength);
             }
             break;
         case WStype_BIN:
-            Serial.printf("[WSc] get binary length: %u\n", length);
-            hexdump(payload, length);
+            Serial.printf("[WSc] get binary length: %u\n", eventLength);
+            hexdump(payload, eventLength);
 
             // send data to server
-            // webSocket.sendBIN(payload, length);
+            // webSocket.sendBIN(payload, eventLength);
             break;
     }
 }
@@ -90,13 +87,13 @@ const String getEventPayload(const String msg) {
   return result;
 }
 
-void trigger(const char* event, const char * payload, size_t length) {
+void trigger(const char* event, const char * payload, size_t triggerLength) {
   Serial.printf("[WSc] trigger event %s\n", event);
   
   if(strcmp(event, "rainbow") == 0) {
     Serial.printf("[WSc] trigger event %s\n", event);
     currentMode = modeRainbow;
-//    shirtColors(payload, length);
+
   } else if (strcmp(event, "colorWipe") == 0){
      Serial.printf("[WSc] trigger event %s\n", event);
      currentMode = modeColorWipe;
@@ -106,48 +103,18 @@ void trigger(const char* event, const char * payload, size_t length) {
   } else if (strcmp(event, "rain") == 0){
      Serial.printf("[WSc] trigger event %s\n", event);
      currentMode = modeRain;
+  } else if (strcmp(event, "sparkle") == 0){
+     Serial.printf("[WSc] trigger event %s\n", event);
+     currentMode = modeSparkle;
   }
 }
 
-void shirtColors(const char * colors, size_t length) {
-  char * pEnd;
-  int number = (int) strtol(colors, &pEnd, 16);
-  int r = number >> 16;
-  int g = number >> 8 & 0xFF;
-  int b = number & 0xFF;
-
-  int number2 = (int) strtol(pEnd, &pEnd, 16);
-  int r2 = number2 >> 16;
-  int g2 = number2 >> 8 & 0xFF;
-  int b2 = number2 & 0xFF;
-}
-
-int brightness = 100;
 void setup() {  
-  strip_1.begin();
-  strip_2.begin();
-  strip_3.begin();
-  strip_4.begin();
-  strip_5.begin();
-  strip_6.begin();
-  strip_7.begin();
-  strip_8.begin();  
-  strip_1.setBrightness(brightness); //adjust brightness here
-  strip_2.setBrightness(brightness); //adjust brightness here
-  strip_3.setBrightness(brightness); //adjust brightness here
-  strip_4.setBrightness(brightness); //adjust brightness here
-  strip_5.setBrightness(brightness); //adjust brightness here
-  strip_6.setBrightness(brightness); //adjust brightness here
-  strip_7.setBrightness(brightness); //adjust brightness here
-  strip_8.setBrightness(brightness); //adjust brightness here
-  strip_1.show(); // Initialize all pixels to 'off'
-  strip_2.show(); // Initialize all pixels to 'off'}
-  strip_3.show(); // Initialize all pixels to 'off'}
-  strip_4.show(); // Initialize all pixels to 'off'}
-  strip_5.show(); // Initialize all pixels to 'off'}
-  strip_6.show(); // Initialize all pixels to 'off'}
-  strip_7.show(); // Initialize all pixels to 'off'}
-  strip_8.show(); // Initialize all pixels to 'off'}
+  for(int x=0; x < stripCount; x++) {
+      pixelStrips[x].begin();
+      pixelStrips[x].setBrightness(brightness);
+      pixelStrips[x].show();
+  }
   
   Serial.begin(115200); // Get ready for serial communications and display the connection status
   Serial.print("Connecting to WiFi network -  ");
@@ -168,9 +135,11 @@ void setup() {
   webSocket.beginSocketIO("led-umbrella.herokuapp.com", 80);
   isConnected = true;   
   webSocket.onEvent(webSocketEvent);
+  delay(500);
 }
 
 void loop() {
+  Serial.printf("loop heap size: %u\n", ESP.getFreeHeap());
   webSocket.loop();
   
   switch(currentMode)
@@ -181,7 +150,7 @@ void loop() {
       break;
     case modeRainbow:
       Serial.print("rainbow\n");
-      colorWipe(strip_1.Color(255, 0, 0, 0), 50); // Red
+      rainbow(0);
       break;
     case modeRain:
       Serial.print("rain\n");
@@ -190,6 +159,10 @@ void loop() {
     case modeRainbowRain:
       Serial.print("rainbow rain\n");
       rainbowRain();
+      break;
+    case modeSparkle:
+      Serial.print("sparkle\n");
+      sparkle();
       break;
     default:
       break;
@@ -213,14 +186,33 @@ void loop() {
       }
    } else {
       Serial.printf("[WSc] Reconnected!\n");
-      WiFi.begin(ssid, password);
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+      }
       delay(10);
       webSocket.beginSocketIO("led-umbrella.herokuapp.com", 80);
       isConnected = true;   
       currentMode = modeSocketConnect;
     }    
 }
- 
+
+void showStrips() {
+  for(int x=0; x < stripCount; x++) {
+      pixelStrips[x].show();
+  }
+}
+
+// draw lights according to strips[][] array
+void updateStrips() {
+  for(int x=0; x < stripCount; x++) {
+    for(int y=ledCount-1; y>=0; y--) {
+      pixelStrips[x].setPixelColor(y, strips[x][y]);
+      pixelStrips[x].show();
+    }
+  }  
+}
+
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
   for(uint16_t i=0; i<strip_1.numPixels(); i++) {
@@ -237,76 +229,42 @@ void colorWipe(uint32_t c, uint8_t wait) {
   }
 }
 
-void showStrips() {
-  strip_1.show();
-  strip_2.show();
-  strip_3.show();
-  strip_4.show();
-  strip_5.show();
-  strip_6.show();
-  strip_7.show();
-  strip_8.show();
+// MR SPARKLE PROGRAM!!!!
+// Draw a random led, remove a random led.
+void sparkle() {
+  int x = random(8);
+  int y = random(17);
+  strips[x][y] = strip_1.Color(255, 255, 255);;
+
+  x = random(8);
+  y = random(17);
+  strips[x][y] = 0;
+
+  updateStrips();
 }
 
 // Rain Program
 void rain() {
+  delay(10);
   uint32_t color = strip_1.Color(0, 0, 255);
-  delay(15);
-  // first move any ON lights down one on each strip
-  for(int x=0; x < stripCount; x++) {
-    for(int y=ledCount-1; y>0; y--) {
-      if (strips[x][y-1] != 0) {
-          strips[x][y] = DimColor2(strips[x][y-1], .75);//strips[x][y-1];
-          
-      }
-      else {
-          strips[x][y] = 0;  
-      }
-    }
-    // each row: special case turn off all first lights
-    strips[x][0] = 0;
-  }
-  // turn on light at first position of random strip
-  strips[random(stripCount)][0] = color;
-
-  // draw lights according to strips[][] array
-  for(int x=0; x < stripCount; x++) {
-    for(int y=ledCount-1; y>=0; y--) {
-      pixelStrips[x].setPixelColor(y, strips[x][y]);
- 
-
-      
-//      pixelStrips[x].setPixelColor(y-1, strips[x][y-1]);
-//      strips[x][y-1]=DimColor(strips[x][y]);
-//      pixelStrips[x].setPixelColor(y-1, strips[x][y-1]);
-//      
-//      strips[x][y-2]=DimColor(strips[x][y-1]);
-//      pixelStrips[x].setPixelColor(y-2, strips[x][y-2]);
-//
-//      strips[x][y-3]=DimColor(strips[x][y-2]);
-//      pixelStrips[x].setPixelColor(y-3, strips[x][y-3]);
-//      
-//      strips[x][y-4]=DimColor(strips[x][y-3]);
-//      pixelStrips[x].setPixelColor(y-4, strips[x][y-4]);
-      
-      pixelStrips[x].show();
-    }
-  }
-  
+  createRain(color);
 }
 
 // Rainbow Rain Program
 void rainbowRain() {
-  uint32_t color = strip_1.Color(random(200), random(200), random(200));
   delay(25);
+  uint32_t color = strip_1.Color(random(255), random(255), random(255));
+  createRain(color);
+}
+
+// Base Rain Program
+void createRain(uint32_t color) {
   // first move any ON lights down one on each strip
   for(int x=0; x < stripCount; x++) {
     for(int y=ledCount-1; y>0; y--) {
       if (strips[x][y-1] != 0) {
           strips[x][y] = strips[x][y-1];
-      }
-      else {
-          strips[x][y] = 0;  
+          strips[x][y-1] = DimColor(strips[x][y-1], .50);  
       }
     }
     // each row: special case turn off all first lights
@@ -315,14 +273,35 @@ void rainbowRain() {
   // turn on light at first position of random strip
   strips[random(stripCount)][0] = color;
 
-  // draw lights according to strips[][] array
-  for(int x=0; x < stripCount; x++) {
-    for(int y=ledCount-1; y>=0; y--) {
-      pixelStrips[x].setPixelColor(y, strips[x][y]);
+  updateStrips();
+}
+
+//Rainbow Program
+void rainbow(uint8_t wait) {
+  int j, x, y;
+  
+  for(j=1; j < 256; j++) {
+    for(x=0; x < stripCount; x++) {
+      for(y=ledCount-1; y>=0; y--) {   
+        pixelStrips[x].setPixelColor(y, Wheel(j));
+      }
       pixelStrips[x].show();
     }
+  }  
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(int WheelPos) {
+  if(WheelPos < 85) {
+    return strip_1.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip_1.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+    WheelPos -= 170;
+    return strip_1.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
-  
 }
 
 // Returns the Red component of a 32-bit color
@@ -343,15 +322,8 @@ uint8_t Blue(uint32_t color)
     return color & 0xFF;
 }
 
-// Return color, dimmed by 75% (used by scanner)
-uint32_t DimColor(uint32_t color)
-{
-    uint32_t dimColor = strip_1.Color(Red(color) >> 1, Green(color) >> 1, Blue(color) >> 1);
-    return dimColor;
-}
-
-// Return color, dimmed by 75% (used by scanner)
-uint32_t DimColor2(uint32_t color, float dimPercent)
+// Return color, dimmed by percentage
+uint32_t DimColor(uint32_t color, float dimPercent)
 {
   int red = Red(color) * dimPercent;
   int blue = Blue(color) * dimPercent;
@@ -359,43 +331,3 @@ uint32_t DimColor2(uint32_t color, float dimPercent)
   uint32_t dimColor = strip_1.Color(red, green, blue);
   return dimColor;
 }
-
-
-//Rainbow Program
-//void rainbow(uint8_t wait) {
-//  uint16_t i, j;
-//  
-//  for(j=0; j<256; j++) {
-//    for(i=0; i<strip.numPixels(); i++) {
-//strip.setPixelColor(i, Wheel((i+j) & 255));
-//}
-//strip.show();
-//    delay(wait);
-//  }
-//}
-//  
-//// Rainbow Cycle Program - Equally distributed
-//void rainbowCycle(uint8_t wait) {
-//uint16_t i, j;
-//  
-//for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-//for(i=0; i< strip.numPixels(); i++) {
-//strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-//}
-//strip.show();
-//delay(wait);
-//}
-//}
-//// Input a value 0 to 255 to get a color value.
-//// The colours are a transition r - g - b - back to r.
-//uint32_t Wheel(byte WheelPos) {
-//if(WheelPos < 85) {
-//return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-//} else if(WheelPos < 170) {
-//WheelPos -= 85;
-//return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-//} else {
-//WheelPos -= 170;
-//return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-//}
-//}
